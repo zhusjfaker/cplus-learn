149377af94293684fefac84db657fd7a4f6252d4
朱仕杰 <zhushijie@zhushijiedeMacBook-Pro-2.local>
2020-03-15 01:02:50 +0800

    zhushijie

--- basetype/index.cpp
+++ basetype/index.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <array>
+#include <vector>
 
 using namespace std;
 
@@ -13,6 +15,11 @@ void print(string c)
     cout << "字符串为: " << c << endl;
 }
 
+void print(char c)
+{
+    cout << "字符串为: " << c << endl;
+}
+
 int main()
 {
     string name = "tome";
@@ -21,5 +28,17 @@ int main()
     int z = stoi(x);
     print(z);
     print(x);
+    char xyz[] = "asdajskdahsd";
+    print(xyz);
+    stpcpy(xyz, "666666666");
+    print(xyz);
+    vector<int> vlist;
+    vlist.push_back(1);
+    vlist.push_back(2);
+    for (int i = 0; i < vlist.size(); i++)
+    {
+        cout << vlist[i] << ",";
+    }
+    array<int, 10> list = {10, 20};
     return 0;
 }
