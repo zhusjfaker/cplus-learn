568c9cddffa7104cdeae3c0f50f210213c6742dc
zhushijie.jayson <zhushijie.jayson@bytedance.com>
2020-04-29 15:09:32 +0800

    jayson.zsj

--- count/index.cpp
+++ count/index.cpp
@@ -1,25 +1,30 @@
 #include <stdio.h>
 #include <string>
+#define emcc false
+
+#if emcc == true
 #include <emscripten.h>
+#endif
 
 using namespace std;
 
 int countplus()
 {
-    return 6666;
+  return 6666;
 }
-
 
 int main()
 {
-    return 0;
+  return 0;
 }
 
+#if emcc == true
 extern "C"
 {
-    EMSCRIPTEN_KEEPALIVE
-    int count()
-    {
-        return countplus();
-    }
+  EMSCRIPTEN_KEEPALIVE
+  int count()
+  {
+    return countplus();
+  }
 }
+#endif
